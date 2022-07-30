fc447bb7c62ac67cec98287cb76528311b43ad3a
Elona Kajtazi <MY_NAME@example.com>
2022-07-30 18:29:07 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,141 @@
+import { useState } from "react";
+
+import initialEmails, { Email } from "./data/emails";
+
+import "./App.css";
+// Instructions
+
+// - Break down App.tsx into components
+//     - You should have an Emails component that renders a list
+//     - You should have an Email component that is a list item
+// - Break down app.css into stylesheets
+// - Pass through the relevant data as props to each component
+// - Pass through the relevant functions as props to each component
+// - Get the search input to work in the header section so that users can search for emails by title
+
+const getReadEmails = (emails: Email[]) =>
+  emails.filter((email) => !email.read);
+
+const getStarredEmails = (emails: Email[]) =>
+  emails.filter((email) => email.starred);
+
+function App() {
+  const [emails, setEmails] = useState(initialEmails);
+  const [hideRead, setHideRead] = useState(false);
+  const [currentTab, setCurrentTab] = useState("inbox");
+
+  const unreadEmails = emails.filter((email) => !email.read);
+  const starredEmails = emails.filter((email) => email.starred);
+
+  const toggleStar = (targetEmail: Email) => {
+    const updatedEmails = (emails: Array<Email>) =>
+      emails.map((email) =>
+        email.id === targetEmail.id
+          ? { ...email, starred: !email.starred }
+          : email
+      );
+    setEmails(updatedEmails);
+  };
+
+  const toggleRead = (targetEmail: Email) => {
+    const updatedEmails = (emails: Array<Email>) =>
+      emails.map((email) =>
+        email.id === targetEmail.id ? { ...email, read: !email.read } : email
+      );
+    setEmails(updatedEmails);
+  };
+
+  function getFilteredEmails(): Array<Email> {
+    let filteredEmails = emails;
+
+    if (hideRead) {
+      filteredEmails = getReadEmails(filteredEmails);
+    }
+
+    if (currentTab === "starred") {
+      filteredEmails = getStarredEmails(filteredEmails);
+    }
+
+    return filteredEmails;
+  }
+
+  return (
+    <div className="app">
+      <header className="header">
+        <div className="left-menu">
+          <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
+            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+          </svg>
+
+          <img
+            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
+            alt="gmail logo"
+          />
+        </div>
+
+        <div className="search">
+          <input className="search-bar" placeholder="Search mail" />
+        </div>
+      </header>
+      <nav className="left-menu">
+        <ul className="inbox-list">
+          <li
+            className={`item ${currentTab === "inbox" ? "active" : ""}`}
+            onClick={() => setCurrentTab("inbox")}
+          >
+            <span className="label">Inbox</span>
+            <span className="count">{unreadEmails.length}</span>
+          </li>
+          <li
+            className={`item ${currentTab === "starred" ? "active" : ""}`}
+            onClick={() => setCurrentTab("starred")}
+          >
+            <span className="label">Starred</span>
+            <span className="count">{starredEmails.length}</span>
+          </li>
+
+          <li className="item toggle">
+            <label htmlFor="hide-read">Hide read</label>
+            <input
+              id="hide-read"
+              type="checkbox"
+              checked={hideRead}
+              onChange={(e) => setHideRead(e.target.checked)}
+            />
+          </li>
+        </ul>
+      </nav>
+      <main className="emails">
+        <ul>
+          {getFilteredEmails().map((email, index) => (
+            <li
+              key={index}
+              className={`email ${email.read ? "read" : "unread"}`}
+            >
+              <div className="select">
+                <input
+                  className="select-checkbox"
+                  type="checkbox"
+                  checked={email.read}
+                  onChange={() => toggleRead(email)}
+                />
+              </div>
+              <div className="star">
+                <input
+                  className="star-checkbox"
+                  type="checkbox"
+                  checked={email.starred}
+                  onChange={() => toggleStar(email)}
+                />
+              </div>
+              <div className="sender">{email.sender}</div>
+              <div className="title">{email.title}</div>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
